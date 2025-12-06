0f4eb62d8df87247eb276460e8d4e14e36da3e13
Fan-Ruixuan <2260174294@qq.com>
2025-12-06 20:03:56 +0800

    fix:添加TurboPack根目录配置

--- next.config.js
+++ next.config.js
@@ -1,1 +1,9 @@
 module.exports = { reactStrictMode: true } 
+const nextConfig = {
+    expeirmrntal: {
+        turbo:{
+            root:__dirname,
+        },
+    },
+}
+module.exports = nextConfig;
